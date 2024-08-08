dc928239fe1519a8c2d38d0b13ec764ccbb74679
James Eldridge <jeldridge@turbosquid.com>
2024-08-08 14:24:29 -0500

    initial commit

---
+++ app/ui/fonts.js
@@ -0,0 +1,20 @@
+import {
+  Courgette,
+  Dancing_Script,
+  Fondamento,
+  Inter,
+  Open_Sans,
+  Salsa,
+  Zen_Loop,
+} from "next/font/google";
+
+export const courgette = Courgette({ subsets: ["latin"], weight: "400" });
+export const dancingScript = Dancing_Script({
+  subsets: ["latin"],
+  weight: "400",
+});
+export const fondamento = Fondamento({ subsets: ["latin"], weight: "400" });
+export const inter = Inter({ subsets: ["latin"] });
+export const openSans = Open_Sans({ subsets: ["latin"], weight: "400" });
+export const salsa = Salsa({ subsets: ["latin"], weight: "400" });
+export const zenLoop = Zen_Loop({ subsets: ["latin"], weight: "400" });
