7a31e7fa845f15e8a671868149bb59bf6313a5cb
James Eldridge <jeldridge@turbosquid.com>
2024-08-09 11:27:41 -0500

    add embla carousel, first pass

---
+++ components/carousel.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { useEffect } from "react";
+import Image from "next/image";
+
+export function EmblaCarousel() {
+  // const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()); // Access API
+    }
+  }, [emblaApi]);
+
+  return (
+    <section ref={emblaRef} className="embla">
+      <div className="embla__container">
+        <div className="embla__slide">
+          <img
+            alt="quilt"
+            className=""
+            // height={1080}
+            src="/quilt.jpg"
+            // width={1920}
+          />
+        </div>
+        <div className="embla__slide">
+          <img
+            alt="quilt"
+            className=""
+            // height={1080}
+            src="/horse.jpg"
+            // width={1920}
+          />
+        </div>
+        <div className="embla__slide">
+          <img
+            alt="quilt"
+            className=""
+            // height={1080}
+            src="/goat.jpg"
+            // width={1920}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
