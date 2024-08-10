eaff925e797abed9ca3967a780acc83c61bb3acd
James Eldridge <jeldridge@turbosquid.com>
2024-08-10 11:19:03 -0500

    add hero component

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,5 +1,9 @@
-import { EmblaCarousel } from "../carousel";
+import { EmblaOptionsType } from "embla-carousel";
+
+import EmblaCarousel from "../carousel";
+
+const OPTIONS: EmblaOptionsType = { loop: true };
 
 export const Hero = () => {
-  return <EmblaCarousel />;
+  return <EmblaCarousel options={OPTIONS} />;
 };
