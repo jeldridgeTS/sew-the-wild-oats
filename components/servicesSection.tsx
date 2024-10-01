fe76d817f3180e6f6bf912df272b5585430df2e2
James Eldridge <jeldridge@turbosquid.com>
2024-09-30 21:20:05 -0500

    add block out section for services

---
+++ components/servicesSection.tsx
@@ -0,0 +1,7 @@
+export default function ServicesSection() {
+  return (
+    <section className="min-h-screen bg-primary-200">
+      <h1 className="text-4xl text-center pt-12">Services</h1>
+    </section>
+  );
+}
