d9f52491be639dc49bd1578b52f3ecafaf1f5fca
James Eldridge <jeldridge@turbosquid.com>
2024-09-30 21:20:37 -0500

    refactor layout to use footer and service section components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -8,6 +7,8 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import Footer from "@/components/footer";
+import ServicesSection from "@/components/servicesSection";
 
 export const metadata: Metadata = {
   title: {
@@ -45,17 +46,8 @@ export default function RootLayout({
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container flex-grow max-w-full">{children}</main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer>
+            <ServicesSection />
+            <Footer />
           </div>
         </Providers>
       </body>
