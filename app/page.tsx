247c5901727c77b348a8acdf321a343f05669bb7
James Eldridge <jeldridge@turbosquid.com>
2024-12-16 11:58:02 -0600

    simplify hero component, refactor/remove section tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import { Hero } from "@/components/hero";
 
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <Hero />
-    </section>
-  );
+  return <Hero />;
 }
 
 {
