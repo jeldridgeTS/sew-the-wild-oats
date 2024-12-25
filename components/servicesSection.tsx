83faa2243ddd622bffbba2142fd6bd1d8803bbd8
James Eldridge <jeldridge@turbosquid.com>
2024-12-25 07:00:26 -0600

    add tote icon svg, refactor services component, product card, temp changes

--- components/servicesSection.tsx
+++ components/servicesSection.tsx
@@ -1,49 +1,17 @@
 "use client";
 
-import { Button, Card, CardFooter, CardHeader, Image } from "@nextui-org/react";
+import ProductCard from "@/components/productCard";
+import { fontCaladea } from "@/config/fonts";
 
 export default function ServicesSection() {
   return (
-    <section className="min-h-screen bg-primary-200">
+    <section className={`${fontCaladea.className} min-h-screen bg-primary-200`}>
       <h1 className="text-4xl text-center pt-12">Services</h1>
-      <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-        <Card
-          isFooterBlurred
-          className="w-full h-[300px] col-span-12 sm:col-span-7"
-        >
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Your day your way
-            </p>
-            <h4 className="text-white/90 font-medium text-xl">
-              Your checklist for better sleep
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Relaxing app background"
-            className="z-0 w-full h-full object-cover"
-            src="https://nextui.org/images/card-example-5.jpeg"
-          />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <Image
-                alt="Breathing app icon"
-                className="rounded-full w-10 h-11 bg-black"
-                src="https://nextui.org/images/breathing-app-icon.jpeg"
-              />
-              <div className="flex flex-col">
-                <p className="text-tiny text-white/60">Breathing App</p>
-                <p className="text-tiny text-white/60">
-                  Get a good nights sleep.
-                </p>
-              </div>
-            </div>
-            <Button radius="full" size="sm">
-              Get App
-            </Button>
-          </CardFooter>
-        </Card>
+
+      <div className={`flex gap-x-24 px-24`}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   );
